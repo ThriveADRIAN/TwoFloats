251f790b88d3fdd0cf0f3986eefbf42d544f617d
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-05 10:57:14 -0400

    Initial Commit

---
+++ TwoFloats/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Adrian Bruce Cunanan on 4/5/13.
+//  Copyright (c) 2013 ThriveStreams. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
